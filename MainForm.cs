8c6f91bab120548c8bbff3ab89ea3d5b2b7bb110
Ryo Kanbayashi <ryo.contact@gmail.com>
2018-10-15 20:07:57 +0900

    add

---
+++ MainForm.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Forms;
+
+class Program
+{
+    static void Main()
+    {
+        Form fm = new Form();
+        Karna.Magnification.Magnifier mag = new Karna.Magnification.Magnifier(fm);
+        Application.Run(fm);
+    }
+}
